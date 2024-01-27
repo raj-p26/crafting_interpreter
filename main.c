ef947fb94717d25e3b2da6db7ef5fc80f98f42cf
raj-p26 <rajpeshavariya26@gmail.com>
2024-01-27 09:59:02 +0530

    Chapter 15 Completed.

--- main.c
+++ main.c
@@ -1,18 +1,69 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char** argv) {
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
 
+    /*
     int constant = addConstant(&chunk, 1.2);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
 
+    constant = addConstant(&chunk, 3.4);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_ADD, 123);
+
+    constant = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_DIVIDE, 123);
+    writeChunk(&chunk, OP_NEGATE, 123);
+    */
+
+    int constant = addConstant(&chunk, 1);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    constant = addConstant(&chunk, 2);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    writeChunk(&chunk, OP_ADD, 1);
+
+    constant = addConstant(&chunk, 3);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    writeChunk(&chunk, OP_MULTIPLY, 1);
+
+    constant = addConstant(&chunk, 4);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    writeChunk(&chunk, OP_SUBTRACT, 1);
+
+    constant = addConstant(&chunk, 5);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    writeChunk(&chunk, OP_NEGATE, 1);
+
+    writeChunk(&chunk, OP_DIVIDE, 1);
+
     writeChunk(&chunk, OP_RETURN, 123);
 
     disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
+
     return 0;
 }
